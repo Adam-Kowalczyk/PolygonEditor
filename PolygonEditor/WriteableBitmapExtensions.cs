40c3c47a6558eb87421e8527fa7767f6a96af81a
Adam Kowalczyk <adamq11@gmail.com>
2019-10-28 21:52:05 +0100

    Line Algorithm changes

--- PolygonEditor/WriteableBitmapExtensions.cs
+++ PolygonEditor/WriteableBitmapExtensions.cs
@@ -11,6 +11,7 @@ namespace PolygonEditor
 {
     public static class WriteableBitmapExtensions
     {
+        public static bool Antialiasing = false;
         public static void DrawPixel(this WriteableBitmap wb, int x, int y, Color color)
         {
             if (x < 0 || x >= wb.PixelWidth) return;
@@ -47,127 +48,91 @@ namespace PolygonEditor
             
         }
 
-        //public static void DrawLine(this WriteableBitmap wb, int x1, int y1, int x2, int y2, Color color)
-        //{
-        //    var reverseX = 1;
-        //    var reverseY = 1;
-        //    if (x1 > x2)
-        //        reverseX = -1;
-        //    if (y1 > y2)
-        //        reverseY = -1;
-
-        //    wb.DrawLineBresenham(reverseX * x1, reverseY * y1, reverseX * x2, reverseY * y2, color, reverseX == -1, reverseY == -1);
-        //}
-
-        static void DrawLineBresenham(this WriteableBitmap wb, int x1, int y1, int x2, int y2, Color color, bool reversedX = false, bool reversedY = false)
-        {
-            int dx = x2 - x1;
-            int dy = y2 - y1;
-            int d = 2 * dy - dx;
-            int incrE = 2 * dy;
-            int incrNE = 2 * (dy - dx);
-            int x = x1;
-            int y = y1;
-            wb.DrawPixel(reversedX ? -x : x, reversedY ? -y : y, color);
-            while (x < x2)
-            {
-                if (d < 0)
-                {
-                    d += incrE;
-                    x++;
-                }
-                else
-                {
-                    d += incrNE;
-                    x++;
-                    y++;
-                }
-                wb.DrawPixel(reversedX ? -x : x, reversedY ? -y : y, color);
-            }
-        }
-
-        static void DrawLineBresenhamLow(this WriteableBitmap wb, int x1, int y1, int x2, int y2, Color color)
-        {
-            int dx = x2 - x1;
-            int dy = y2 - y1;
-            int yi = 1;
-            if (dy < 0)
-            {
-                yi = -1;
-                dy = -dy;
-            }
-            int incrE = 2 * dy;
-            int incrNE = 2 * (dy - dx);
-            int d = 2 * dy - dx;
-            int x = x1;
-            int y = y1;
-            while (x < x2)
-            {
-                if (d < 0)
-                {
-                    d += incrE;
-                    x++;
-                }
-                else
-                {
-                    d += incrNE;
-                    x++;
-                    y = y + yi;
-                }
-                wb.DrawPixel(x, y, color);
-            }
-        }
-
-        static void DrawLineBresenhamHigh(this WriteableBitmap wb, int x1, int y1, int x2, int y2, Color color)
-        {
-            int dx = x2 - x1;
-            int dy = y2 - y1;
-            int xi = 1;
-            if (dx < 0)
-            {
-                xi = -1;
-                dx = -dx;
-            }
-            int incrE = 2 * dx;
-            int incrNE = 2 * (dx - dy);
-            int d = 2 * dx - dy;
-            int x = x1;
-            int y = y1;
-            while (y < y2)
-            {
-                if (d < 0)
-                {
-                    d += incrE;
-                    y++;
-                }
-                else
-                {
-                    d += incrNE;
-                    y++;
-                    x = x + xi;
-                }
-                wb.DrawPixel(x, y, color);
-            }
-        }
-
         public static void DrawLine(this WriteableBitmap wb, int x1, int y1, int x2, int y2, Color color)
         {
-            wb.DrawPixel(x1, y1, color);
-            if(Math.Abs(y2-y1) < Math.Abs(x2-x1))
+                wb.DrawBresenhamLine(x1, y1, x2, y2, color);
+        }
+
+        public static void DrawBresenhamLine(this WriteableBitmap wb, int x1, int y1, int x2, int y2, Color color)
+        {
+            //Source: https://pl.wikipedia.org/wiki/Algorytm_Bresenhama
+            // zmienne pomocnicze
+            int d, dx, dy, ai, bi, xi, yi;
+            int x = x1, y = y1;
+            // ustalenie kierunku rysowania
+            if (x1 < x2)
             {
-                if (x1 > x2)
-                    wb.DrawLineBresenhamLow(x2, y2, x1, y1, color);
-                else
-                    wb.DrawLineBresenhamLow(x1, y1, x2, y2, color);
+                xi = 1;
+                dx = x2 - x1;
             }
             else
             {
-                if (y1 > y2)
-                    wb.DrawLineBresenhamHigh(x2, y2, x1, y1, color);
-                else
-                    wb.DrawLineBresenhamHigh(x1, y1, x2, y2, color);
+                xi = -1;
+                dx = x1 - x2;
+            }
+            // ustalenie kierunku rysowania
+            if (y1 < y2)
+            {
+                yi = 1;
+                dy = y2 - y1;
+            }
+            else
+            {
+                yi = -1;
+                dy = y1 - y2;
+            }
+            // pierwszy piksel
+            wb.DrawPixel(x, y, color);
+            // oś wiodąca OX
+            if (dx > dy)
+            {
+                ai = (dy - dx) * 2;
+                bi = dy * 2;
+                d = bi - dx;
+                // pętla po kolejnych x
+                while (x != x2)
+                {
+                    // test współczynnika
+                    if (d >= 0)
+                    {
+                        x += xi;
+                        y += yi;
+                        d += ai;
+                    }
+                    else
+                    {
+                        d += bi;
+                        x += xi;
+                    }
+                    wb.DrawPixel(x, y, color);
+                }
+            }
+            // oś wiodąca OY
+            else
+            {
+                ai = (dx - dy) * 2;
+                bi = dx * 2;
+                d = bi - dy;
+                // pętla po kolejnych y
+                while (y != y2)
+                {
+                    // test współczynnika
+                    if (d >= 0)
+                    {
+                        x += xi;
+                        y += yi;
+                        d += ai;
+                    }
+                    else
+                    {
+                        d += bi;
+                        y += yi;
+                    }
+                    wb.DrawPixel(x, y, color);
+                }
             }
         }
+
 
         public static void DrawRelationBox(this WriteableBitmap wb, int x, int y, Relation rel, int number)
         {
